57adf9348519c4f39245a6f7c035ae4cef1511bc
Jamieson26a <jamieson26a@ncssm.edu>
2025-02-09 12:03:41 -0500

    Add files via upload

---
+++ SMATH_HACKS_PYTHON_YIPPEEEEEE.py
@@ -0,0 +1,27 @@
+# SMATH_HACKS PYTHON YIPPEEEEEE
+
+import sys
+
+from PyQt6.QtCore import QSize, Qt
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
+
+
+# Subclass QMainWindow to customize your application's main window
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("My App")
+        button = QPushButton("Press Me!")
+        button.setFixedSize(QSize(100, 50))
+
+        # Set the central widget of the Window.
+        self.setCentralWidget(button)
+
+
+app = QApplication(sys.argv)
+
+window = MainWindow()
+window.show()
+
+app.exec()
